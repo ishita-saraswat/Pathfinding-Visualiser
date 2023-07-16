a847ab022049e02e713bf922cae1db8bfdfadb9d
Ishita Saraswat <104856170+ishita-saraswat@users.noreply.github.com>
2023-07-16 12:08:37 +0530

    Update Visualizer.js

--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import Node from './Node';
 import './Visualizer.css';
 
